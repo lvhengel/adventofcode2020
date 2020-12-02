a1f9f14e82f8afb67df181c7d1cae697b64bf533
Leo van Hengel <leo.van.hengel@gmail.com>
2020-12-02 07:34:20 +0100

    remove withIndex

--- src/day01/day01.kts
+++ src/day01/day01.kts
@@ -4,11 +4,12 @@
 
 fun part1() {
     val input = readLines("input.txt").map(String::toInt)
-    for ((index1, value1) in input.withIndex()) {
-        for ((index2, value2) in input.withIndex()) {
+    for (value1 in input) {
+        for (value2 in input) {
             if (value1 + value2 === 2020) {
-                println("Values are $value1 and $value2")
-                println("Sum: " + value1 * value2)
+                println("Part 1:\nvalues are $value1 and $value2")
+                println("sum: " + value1 * value2)
+                return
             }
         }
     }
@@ -16,12 +17,13 @@ fun part1() {
 
 fun part2() {
     val input = readLines("input.txt").map(String::toInt)
-    for ((index1, value1) in input.withIndex()) {
-        for ((index2, value2) in input.withIndex()) {
-            for ((index3, value3) in input.withIndex()) {
+    for (value1 in input) {
+        for (value2 in input) {
+            for (value3 in input) {
                 if (value1 + value2 + value3 === 2020) {
-                    println("Values are $value1,$value2,$value3")
-                    println("Sum: " + value1 * value2 * value3)
+                    println("Part 2:\nvalues are $value1,$value2,$value3")
+                    println("sum: " + value1 * value2 * value3)
+                    return
                 }
             }
         }
